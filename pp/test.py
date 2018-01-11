4528852a4440aa5566a262d4577b0a3efa63c2d3
黃禹傑 <bigmolemushroom@huangyuketsutekiMacBook-Air.local>
2018-01-11 18:31:30 +0800

    test

--- pp/test.py
+++ pp/test.py
@@ -4,47 +4,56 @@ import cv2
 from picamera import PiCamera
 from picamera.array import PiRGBArray 
 
-class Test:
-    def __init__(self):
-        self.camera = PiCamera()
-        self.camera.resolution = (1216, 960)
-    def getIm(self):
-        output = np.empty((1216*960*3), dtype = np.uint8)
-        self.camera.capture(output, 'bgr')
-        output = output.reshape(1216, 960, 3)
-        output = output[:1200, :960, ]
+self.camera = PiCamera()
+self.camera.resolution = (1216, 960)
 
-        return output
+output = np.empty((1216*960*3), dtype = np.uint8)
+self.camera.capture(output, 'bgr')
+output = output.reshape(1216, 960, 3)
+output = output[:1200, :960, ]
 
-    def getPoint(self, im):
-        # Get the masked image
-        im_hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-        # lower mask
-        lowRed = np.array([0,50,50])
-        uppRed = np.array([10,255,255])
-        im_mask0 = cv2.inRange(im_hsv, lowRed, uppRed)
-        # upper mask
-        lowRed = np.array([170,50,50])
-        uppRed = np.array([180,255,255])
-        im_mask1 = cv2.inRange(im_hsv, lowRed, uppRed)
-        # join the masks
-        im_mask = im_mask0 + im_mask1
-        # Get the non-zero part
-        location = cv2.findNonZero(im_mask)
-        if(location is None):
-            return[-1, -1]
-        pixelNum = len(location)
-        x_ave = y_ave = 0
-        for i in range(pixelNum):
-            x_ave += location[i][0][0]
-            y_ave += location[i][0][1]
-        
-        return [x_ave // pixelNum, y_ave // pixelNum]
+im = output
+x = y = 0
+
+# Get the masked image
+im_hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+# lower mask
+lowRed = np.array([0,50,50])
+uppRed = np.array([10,255,255])
+im_mask0 = cv2.inRange(im_hsv, lowRed, uppRed)
+# upper mask
+lowRed = np.array([170,50,50])
+uppRed = np.array([180,255,255])
+im_mask1 = cv2.inRange(im_hsv, lowRed, uppRed)
+# join the masks
+im_mask = im_mask0 + im_mask1
+# Get the non-zero part
+location = cv2.findNonZero(im_mask)
+if(location is None):
+    x = y = -1
+else:
+    pixelNum = len(location)
+    x_ave = y_ave = 0
+    for i in range(pixelNum):
+        x_ave += location[i][0][0]
+        y_ave += location[i][0][1]
+    x = x_ave // pixelNum
+    y = y_ave // pixelNum
+
+print('(', x, ', ', y, ')')
+
+cv2.imwrite('im.jpg', im)
+cv2.imwrite('im_hsv.jpg', im_hsv)
+cv2.imwrite('mask.jpg', im_mask)
 
 
-t = Test()
-while(True):
-    im = t.getIm()
-    x, y = t.getPoint(im)
-    print('(', x, ', ', y, ')')
-    input
+
+
+
+
+
+
+
+
+
+
