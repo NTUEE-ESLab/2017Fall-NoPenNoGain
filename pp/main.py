8547e86b57ad52c50674139c32c63b7d65113b9c
黃禹傑 <bigmolemushroom@gmail.com>
2018-01-11 15:37:11 +0800

    debug

--- pp/main.py
+++ pp/main.py
@@ -27,11 +27,11 @@ class TrackCam:
         # lower mask
         lowRed = np.array([0,50,50])
         uppRed = np.array([10,255,255])
-        im_mask0 = cv2.inRange(img_hsv, lowRed, uppRed)
+        im_mask0 = cv2.inRange(im_hsv, lowRed, uppRed)
         # upper mask
         lowRed = np.array([170,50,50])
         uppRed = np.array([180,255,255])
-        im_mask1 = cv2.inRange(img_hsv, lowRed, uppRed)
+        im_mask1 = cv2.inRange(im_hsv, lowRed, uppRed)
         # join the masks
         im_mask = im_mask0 + im_mask1
         # Get the non-zero part
