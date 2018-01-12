7a06cd39b4cfc3e0751a2016b8b4222a326e3985
b04901142 <b04901142@ntu.edu.tw>
2018-01-12 12:30:02 +0800

    pen

--- pen/main.js
+++ pen/main.js
@@ -1,17 +1,72 @@
 // create an instance of the rpio-gpio-buttons object with pins 11 and 13 
 var buttons = require('rpi-gpio-buttons')([11, 13]);
- 
+
+var bleno = require('../..');
+
+var BlenoPrimaryService = bleno.PrimaryService;
+
+var EchoCharacteristic = require('./characteristic');
+
+var right = '0'
+
+var left = '0'
+
+console.log('bleno - echo');
+
+bleno.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('echo', ['ec00']);
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function(error) {
+  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+  if (!error) {
+    bleno.setServices([
+      new BlenoPrimaryService({
+        uuid: 'ec00',
+        characteristics: [
+          new EchoCharacteristic()
+        ]
+      })
+    ]);
+  }
+});
+
 // bind to the clicked event and check for the assigned pins when clicked 
-buttons.on('clicked', function (pin) {
+buttons.on('pressed', function (pin) {
   switch(pin) {
     // Up button on pin 11 was clicked 
     case 11:
-    userClickedUp();
-    break
+    left = '1';
+    EchoCharacteristic._value = Buffer(left + right);
+    break;
  
     // Down button on pin 13 was clicked 
     case 13:
-    userClickedDown();
+    left = '1';
+    EchoCharacteristic._value = Buffer(left + right);
+    break;
+  }
+});
+
+buttons.on('released', function (pin) {
+  switch(pin) {
+    // Up button on pin 11 was clicked 
+    case 11:
+    left = '0';
+    EchoCharacteristic._value = Buffer(left + right);
+    break;
+ 
+    // Down button on pin 13 was clicked 
+    case 13:
+    right = '0';
+    EchoCharacteristic._value = Buffer(left + right);
     break;
   }
 });
