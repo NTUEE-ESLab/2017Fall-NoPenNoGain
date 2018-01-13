9cd50c37c7d2cbd69c20adcc196978cc18d1b91c
b04901142 <b04901142@ntu.edu.tw>
2018-01-13 11:38:39 +0800

    final push

--- c++/trackCam/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ main.cpp -o run $(pkg-config opencv --cflags --libs) -L/opt/vc/lib -I/use/local/include/ -lraspicam -lraspicam_cv -lmmal -lmmal_core -lmmal_util -lopencv_core -lopencv_highgui
